16dfbcf7fd33a197b3e6c11a516aaf6ea0d5db8e
ruhisingh1 <145125444+ruhisingh1@users.noreply.github.com>
2023-12-01 20:59:39 +0530

    Update article-metadata.js

--- blocks/article-metadata/article-metadata.js
+++ blocks/article-metadata/article-metadata.js
@@ -1,26 +1,12 @@
 export default function decorate(block) {
-    const lastUpdateElement = document.querySelector('.article-metadata-wrapper .article-metadata');
-    const lastUpdateText = lastUpdateElement.textContent.trim();
-    // console.log(lastUpdateText); 
-    // console.log(lastUpdateElement)   
-    const datePattern = /Last update: (.+)/;
-    const match = lastUpdateText.match(datePattern);
-    const lastUpdateDate = match ? match[1] : '';
-    // const lastUpdate = Date.parse(lastUpdateDate);
-    var lastUpdate = new Date(lastUpdateDate).toISOString();
-    console.log("lastupdated",lastUpdate);
-    // if (isNaN(lastUpdate) === false) 
-    // {
-    //     console.log("enter")
-    //     console.log(lastUpdate+'T00:00:00Z')
-    //     const formatedDate = new Intl.DateTimeFormat('en', {year: 'numeric', month: 'long', day: 'numeric'}).format(new Date(lastUpdate)); 
-    //     console.log("formateddate",formatedDate)
-    //     lastUpdateElement.innerHTML = `<div><div>Last update: ${formatedDate}</div></div>`
-    // }
-    const date = new Date(lastUpdate); // this you are already getting
-    const options = { month: 'long', day: 'numeric', year: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-US', options);
-    console.log(formattedDate);
-    lastUpdateElement.innerHTML = `<div><div>Last update: ${formattedDate}</div></div>`
-
+  const lastUpdateElement = block.querySelector('.article-metadata-wrapper > div > div > div');
+  const lastUpdateText = lastUpdateElement.textContent.trim();
+  const datePattern = /Last update: (.+)/;
+  const match = lastUpdateText.match(datePattern);
+  const lastUpdateDate = match ? match[1] : '';
+  const lastUpdateISO = new Date(lastUpdateDate).toISOString();
+  const date = new Date(lastUpdateISO);
+  const formatOptions = { month: 'long', day: 'numeric', year: 'numeric' };
+  const formattedDate = date.toLocaleDateString('en-US', formatOptions);
+  lastUpdateElement.innerHTML = `Last update: ${formattedDate}`;
 }
