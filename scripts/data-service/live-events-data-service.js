8c66d221847032b104db968f59e49e745ac027e5
Ruhi Singh <ruhis@adobe.com>
2023-12-08 22:41:36 +0530

    Events cards

--- scripts/data-service/live-events-data-service.js
+++ scripts/data-service/live-events-data-service.js
@@ -8,8 +8,8 @@ export default class LiveEventsDataService {
    * Creates an instance of LiveEventsDataService.
    * @param {Object} dataSource - The data source configuration for Live Events Service.
    */
-  constructor(url) {
-    this.url = url;
+  constructor(dataSource) {
+    this.dataSource = dataSource;
   }
 
   /**
@@ -22,7 +22,8 @@ export default class LiveEventsDataService {
       if (LIVE_EVENTS in sessionStorage) {
         return JSON.parse(sessionStorage[LIVE_EVENTS]);
       }
-      const response = await fetch(this.url, {
+      const urlWithParams = `${this.dataSource.url}?${this.dataSource.param.toString()}`;
+      const response = await fetch(urlWithParams, {
         method: 'GET',
       });
       const data = await response.json();
