6e65ab81990e048ef5bfdc6099c2de321333d7ec
Ruhi Singh <ruhis@adobe.com>
2023-12-18 13:48:00 +0530

    Updated Events Cards

--- blocks/events-cards/events-cards.js
+++ blocks/events-cards/events-cards.js
@@ -3,6 +3,19 @@ import BrowseCardsDelegate from '../../scripts/browse-card/browse-cards-delegate
 import { htmlToElement } from '../../scripts/scripts.js';
 import buildCard from '../../scripts/browse-card/browse-card.js';
 import CONTENT_TYPES from '../../scripts/browse-card/browse-cards-constants.js';
+
+/**
+ * formattedSolutionTags returns the solution type by stripping off the exl:solution/ string
+ * @param {string} inputString - The solution tag. E.g. exl:solution/experience-cloud
+ * @returns the solution tag. E.g. experience-cloud
+ */
+function formattedSolutionTags(inputString) {
+  return inputString
+    .replace(/exl:solution\//g, '')
+    .split(',')
+    .map((part) => part.trim());
+}
+
 /**
  * Decorate function to process and log the mapped data.
  * @param {HTMLElement} block - The block of data to process.
@@ -12,14 +25,10 @@ export default async function decorate(block) {
   const headingElement = block.querySelector('div:nth-child(1) > div');
   const toolTipElement = block.querySelector('div:nth-child(2) > div');
   const linkTextElement = block.querySelector('div:nth-child(3) > div > a');
-  // const allSolutions = block.querySelector('div:nth-child(4) > div').textContent.trim();
   const solutions = block.querySelector('div:nth-child(4) > div').textContent.trim();
   const contentType = CONTENT_TYPES.LIVE_EVENTS.MAPPING_KEY;
   const noOfResults = 4;
-  // eslint-disable-next-line no-use-before-define
   const solutionsParam = solutions !== '' ? formattedSolutionTags(solutions) : '';
-  // If All Solutions toggle is on, solution param will be empty, else use solutions tag as param
-  // const solutionsParam = allSolutions === 'true' ? '' : solutionsTags;
 
   // Clearing the block's content
   block.innerHTML = '';
@@ -36,31 +45,37 @@ export default async function decorate(block) {
     </div>
   `);
   // Appending header div to the block
-  block.appendChild(headerDiv);
+  block.append(headerDiv);
+  block.classList.add('browse-cards-block');
 
-  const param = {
+  const parameters = {
     contentType,
   };
+  
+  const browseCardsContent = BrowseCardsDelegate.fetchCardData(parameters);
+  browseCardsContent
+    .then((data) => {
+      // eslint-disable-next-line no-use-before-define
+      const filteredLiveEventsData = fetchFilteredCardData(data, solutionsParam);
+      if (filteredLiveEventsData?.length) {
+        const contentDiv = document.createElement('div');
+        contentDiv.classList.add('events-cards-content');
 
-  const browseCardsContent = BrowseCardsDelegate.fetchCardData(param);
-  browseCardsContent.then((data) => {
-    // eslint-disable-next-line no-use-before-define
-    const filteredLiveEventsData = fetchFilteredCardData(data, solutionsParam);
-    if (filteredLiveEventsData?.length) {
-      const contentDiv = document.createElement('div');
-      contentDiv.classList.add('events-cards-content');
+        for (let i = 0; i < Math.min(noOfResults, filteredLiveEventsData.length); i += 1) {
+          const cardData = filteredLiveEventsData[i];
+          const cardDiv = document.createElement('div');
+          buildCard(cardDiv, cardData);
+          contentDiv.appendChild(cardDiv);
+        }
 
-      for (let i = 0; i < Math.min(noOfResults, filteredLiveEventsData.length); i += 1) {
-        const cardData = filteredLiveEventsData[i];
-        const cardDiv = document.createElement('div');
-        buildCard(cardDiv, cardData);
-        contentDiv.appendChild(cardDiv);
+        block.appendChild(contentDiv);
+        decorateIcons(block);
       }
-
-      block.appendChild(contentDiv);
-      decorateIcons(block);
-    }
-  });
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error('Events Cards:', err);
+    });
 
   /**
    * fetchFilteredCardData filters the events data based on productFocus key in events JSON
@@ -71,37 +86,28 @@ export default async function decorate(block) {
   const fetchFilteredCardData = (data, params) => {
     const eventData = { data };
     if (eventData.data) {
-      const paramsArray = Array.isArray(params) ? params : [params];
-      if (paramsArray.length === 0 || paramsArray.some((p) => p === '')) {
-        return eventData.data.sort((a, b) => new Date(a.event.startTime) - new Date(b.event.startTime));
-        
+      const solutionsList = Array.isArray(params) ? params : [params];
+      // If solutions param is empty or contains an empty value, return all the results in startTime ascending order
+      if (solutionsList.length === 0 || solutionsList.some((param) => param === '')) {
+        return eventData.data
+          .filter((card) => card.event.startTime)
+          .sort((card1, card2) => new Date(card1.event.startTime) - new Date(card2.event.startTime));
       }
 
-      const lowercaseParams = paramsArray.map((parameter) => parameter.toLowerCase());
-
+      const lowercaseParams = solutionsList.map((parameter) => parameter.toLowerCase());
+      const regex = /[^a-zA-Z0-9().]+/g;
       const filteredData = eventData.data.filter((event) => {
         const productArray = Array.isArray(event.product) ? event.product : [event.product];
-        const lowercaseProduct = productArray.map((item) => item.toLowerCase().replaceAll(' ', '-'));
+        const lowercaseProduct = productArray.map((item) => item.toLowerCase().replaceAll(regex, '-'));
         return lowercaseParams.some((parameter) => lowercaseProduct.includes(parameter.trim()));
       });
-       
+
       // Sort events by startTime in ascending order
-      return filteredData.sort(
-        (card1, card2) => new Date(card1.event.startTime) - new Date(card2.event.startTime),
-      );
+      return filteredData
+        .filter((card) => card.event.startTime)
+        .sort((card1, card2) => new Date(card1.event.startTime) - new Date(card2.event.startTime));
     }
+    // In case of invalid solution param, return empty results.
     return [];
   };
-}
-
-/**
- * formattedSolutionTags returns the solution type by stripping off the exl:solution/ string
- * @param {string} inputString - The solution tag. E.g. exl:solution/experience-cloud
- * @returns the solution tag. E.g. experience-cloud
- */
-function formattedSolutionTags(inputString) {
-  return inputString
-    .replace(/exl:solution\//g, '')
-    .split(',')
-    .map((part) => part.trim());
 }
