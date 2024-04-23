e9f6c5c996b53bd420d17e7171e9d26783c80fc8
Ruhi Singh <ruhis@adobe.com>
2024-04-23 18:05:30 +0530

    EXLM-1376 Page meta-tags for coveo index

--- coveo/generate-coveo-index.js
+++ coveo/generate-coveo-index.js
@@ -14,61 +14,63 @@ function decodeBase64(encodedString) {
 
 // Generic function to decode base64 and remove prefix
 function decodeAndRemovePrefix(value, prefix) {
-    const decodedValue = decodeBase64(value.replace(prefix, ''));
-    return decodedValue;
+  const decodedValue = decodeBase64(value.replace(prefix, ''));
+  return decodedValue;
 }
 
 // Fetch articles
 async function fetchDataFromURL(url) {
-    return new Promise((resolve, reject) => {
-        https.get(url, (response) => {
-            let data = '';
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (response) => {
+        let data = '';
 
-            // A chunk of data has been received.
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            // The whole response has been received.
-            response.on('end', () => {
-                try {
-                    const jsonData = JSON.parse(data);
-                    resolve(jsonData);
-                } catch (error) {
-                    reject(error);
-                }
-            });
-        }).on('error', (error) => {
-            reject(error);
+        // A chunk of data has been received.
+        response.on('data', (chunk) => {
+          data += chunk;
         });
-    });
+
+        // The whole response has been received.
+        response.on('end', () => {
+          try {
+            const jsonData = JSON.parse(data);
+            resolve(jsonData);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
 }
 
 // Main function to generate XML content
 async function generateXmlContent() {
   const url = `https://main--franklin-exlm--ruhisingh1.hlx.page/${language}/article-index.json`;
-    try {
-        const articles = await fetchDataFromURL(url);
-        const xmlData = [];
+  try {
+    const articles = await fetchDataFromURL(url);
+    const xmlData = [];
 
-        articles.data.forEach((article) => { 
-            xmlData.push('<url>');
-            xmlData.push(`  <loc>${article.path}</loc>`);
-            xmlData.push(`  <lastmod>${article.lastModified}</lastmod>`); 
-            xmlData.push('  <changefreq>daily</changefreq>');
-            xmlData.push('  <coveo:metadata>');
-            xmlData.push(`    <coveo-content-type>${article.coveoContentType}</coveo-content-type>`);
-            const decodedSolution = decodeAndRemovePrefix(article.coveoSolution, 'exl:solution/');
-            xmlData.push(`    <coveo-solution><![CDATA[ ${decodedSolution} ]]></coveo-solution>`);
-            const decodedRole = decodeAndRemovePrefix(article.coveoRole, 'exl:role/');
-            xmlData.push(`    <role><![CDATA[ ${decodedRole} ]]></role>`); 
-            const decodedLevel = decodeBase64(decodeAndRemovePrefix(article.coveoLevel, 'exl:experience-level/'));
-            xmlData.push(`    <level><![CDATA[ ${decodedLevel} ]]></level>`); 
-            xmlData.push('  </coveo:metadata>');
-            xmlData.push('</url>');
-        });
+    articles.data.forEach((article) => {
+      xmlData.push('<url>');
+      xmlData.push(`  <loc>${article.path}</loc>`);
+      xmlData.push(`  <lastmod>${article.lastModified}</lastmod>`);
+      xmlData.push('  <changefreq>daily</changefreq>');
+      xmlData.push('  <coveo:metadata>');
+      xmlData.push(`    <coveo-content-type>${article.coveoContentType}</coveo-content-type>`);
+      const decodedSolution = decodeAndRemovePrefix(article.coveoSolution, 'exl:solution/');
+      xmlData.push(`    <coveo-solution>${decodedSolution}</coveo-solution>`);
+      const decodedRole = decodeAndRemovePrefix(article.coveoRole, 'exl:role/');
+      xmlData.push(`    <role>${decodedRole}</role>`);
+      const decodedLevel = decodeBase64(decodeAndRemovePrefix(article.coveoLevel, 'exl:experience-level/'));
+      xmlData.push(`    <level>${decodedLevel}</level>`);
+      xmlData.push('  </coveo:metadata>');
+      xmlData.push('</url>');
+    });
 
-        return `
+    return `
             <urlset xmlns="http://www.google.com/schemas/sitemap/0.84"
                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
                     xmlns:coveo="http://www.coveo.com/schemas/metadata"
@@ -76,33 +78,33 @@ async function generateXmlContent() {
                 ${xmlData.join('\n')}
             </urlset>
         `;
-    } catch (error) {
-      // eslint-disable-next-line no-console
-        console.error('Error generating XML content:', error);
-        throw error;
-    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error generating XML content:', error);
+    throw error;
+  }
 }
 
 // Write Coveo XML file
 async function writeCoveoXML() {
-    try {
-        const xmlContent = await generateXmlContent();
-        const fileName = `coveo_${language}.xml`;
-        fs.writeFileSync(fileName, xmlContent);
-    } catch (error) {
-      // eslint-disable-next-line no-console
-        console.error('Error writing Coveo XML file:', error);
-        throw error;
-    }
+  try {
+    const xmlContent = await generateXmlContent();
+    const fileName = `coveo_${language}.xml`;
+    fs.writeFileSync(fileName, xmlContent);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error writing Coveo XML file:', error);
+    throw error;
+  }
 }
 
 async function main() {
-    try {
-        await writeCoveoXML();
-    } catch (error) {
-      // eslint-disable-next-line no-console
-        console.error('Error:', error);
-    }
+  try {
+    await writeCoveoXML();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error:', error);
+  }
 }
 
 main();
