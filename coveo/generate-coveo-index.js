8a1a9312016f17befa1fffa0207d8c6a3e0e9dd8
Ruhi Singh <ruhis@adobe.com>
2024-04-23 16:52:03 +0530

    EXLM-1376 Page meta-tags for coveo index

--- coveo/generate-coveo-index.js
+++ coveo/generate-coveo-index.js
@@ -52,27 +52,36 @@ async function generateXmlContent() {
       // Access 'data' property of articles
       xmlData.push('<url>');
       xmlData.push(`  <loc>${article.path}</loc>`);
-      xmlData.push(`  <lastmod>${article.lastModified}</lastmod>`);
+      xmlData.push(`  <lastmod>${article.lastModified}</lastmod>`); // Fix property name
       xmlData.push('  <changefreq>daily</changefreq>');
       xmlData.push('  <coveo:metadata>');
       xmlData.push(`    <coveo-content-type>${article.coveoContentType}</coveo-content-type>`);
-      xmlData.push(`    <coveo-solution>${decodeBase64(article.coveoSolution)}</coveo-solution>`);
-      xmlData.push(`    <role>${decodeBase64(article.coveoRole)}</role>`);
-      xmlData.push(`    <level>${decodeBase64(article.coveoLevel)}</level>`);
-      xmlData.push(`    <author-type>${article.authorType}</author-type>`);
-      xmlData.push(`    <author-name>${article.authorName}</author-name>`);
+
+      // Decode base64 strings and remove prefix from coveoSolution
+      const solutions = article.coveoSolution.split(', ');
+      solutions.forEach((solution) => {
+        xmlData.push(`    <coveo-solution>${decodeBase64(solution.replace('exl:solution/', ''))}</coveo-solution>`);
+      });
+
+      // Remove prefix from coveoRole
+      xmlData.push(`    <role>${decodeBase64(article.coveoRole.replace('exl:role/', ''))}</role>`); // Fix property name
+
+      // Decode base64 string and remove prefix from coveoLevel
+      xmlData.push(`    <level>${decodeBase64(article.coveoLevel.replace('exl:experience-level/', ''))}</level>`); // Fix property name
+
+      // If there are other properties to include, add them here
       xmlData.push('  </coveo:metadata>');
       xmlData.push('</url>');
     });
 
     return `
-            <urlset xmlns="http://www.google.com/schemas/sitemap/0.84"
-                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                    xmlns:coveo="http://www.coveo.com/schemas/metadata"
-                    xsi:schemaLocation="http://www.google.com/schemas/sitemap/0.84 http://www.google.com/schemas/sitemap/0.84/sitemap.xsd">
-                ${xmlData.join('\n')}
-            </urlset>
-        `;
+          <urlset xmlns="http://www.google.com/schemas/sitemap/0.84"
+                  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                  xmlns:coveo="http://www.coveo.com/schemas/metadata"
+                  xsi:schemaLocation="http://www.google.com/schemas/sitemap/0.84 http://www.google.com/schemas/sitemap/0.84/sitemap.xsd">
+              ${xmlData.join('\n')}
+          </urlset>
+      `;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Error generating XML content:', error);
