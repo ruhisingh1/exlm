fcdc66b8c8dfff2fc44a2ef9ad893a4a2e3aae87
Ruhi Singh <ruhis@adobe.com>
2024-04-23 13:43:58 +0530

    EXLM-1376 Page meta-tags for coveo index

--- coveo/generate-coveo-index.js
+++ coveo/generate-coveo-index.js
@@ -2,8 +2,11 @@ import fs from 'fs';
 
 // Parse command line arguments
 const args = process.argv.slice(2);
+console.log('Command line arguments:', args); // Debugging statement
 const languageIndex = args.indexOf('--language');
+console.log('Language index:', languageIndex); // Debugging statement
 const language = languageIndex !== -1 ? args[languageIndex + 1] : 'en'; // Default language is 'en'
+console.log('Selected language:', language); // Debugging statement
 
 // Generate Coveo XML content based on language
 const generateCoveoXml = () => {
@@ -20,6 +23,7 @@ const generateCoveoXml = () => {
 // Write Coveo XML file
 const writeCoveoXmlFile = (xmlContent) => {
     const fileName = `coveo_${language}.xml`;
+    console.log('Writing to file:', fileName); // Debugging statement
     fs.writeFileSync(fileName, xmlContent);
     console.log(`Coveo XML file '${fileName}' created successfully.`);
 };
