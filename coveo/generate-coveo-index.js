1f39bbf27377c4021fe9e64d42f923c7e2119225
Ruhi Singh <ruhis@adobe.com>
2024-04-23 14:09:51 +0530

    EXLM-1376 Page meta-tags for coveo index

--- coveo/generate-coveo-index.js
+++ coveo/generate-coveo-index.js
@@ -1,15 +1,12 @@
-/* eslint-disable no-console, no-unused-expressions, no-nested-ternary, quote-props */
 import fs from 'fs';
+import process from 'process'; // Import the process module
 
 // Parse command line arguments
-const args = process.argv.slice(2);
-// eslint-disable-next-line no-console
+const args = process.argv.slice(2); // Access process.argv directly
 console.log('Command line arguments:', args); // Debugging statement
 const languageIndex = args.indexOf('--language');
-// eslint-disable-next-line no-console
 console.log('Language index:', languageIndex); // Debugging statement
 const language = languageIndex !== -1 ? args[languageIndex + 1] : 'en'; // Default language is 'en'
-// eslint-disable-next-line no-console
 console.log('Selected language:', language); // Debugging statement
 
 // Generate Coveo XML content based on language
@@ -27,10 +24,8 @@ const generateCoveoXml = () => {
 // Write Coveo XML file
 const writeCoveoXmlFile = (xmlContent) => {
     const fileName = `coveo_${language}.xml`;
-    // eslint-disable-next-line no-console
     console.log('Writing to file:', fileName); // Debugging statement
     fs.writeFileSync(fileName, xmlContent);
-    // eslint-disable-next-line no-console
     console.log(`Coveo XML file '${fileName}' created successfully.`);
 };
 
