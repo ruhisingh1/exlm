fc72de83dbd1ab64170230fcf211da62efaab29c
Ruhi Singh <ruhis@adobe.com>
2024-04-23 16:42:09 +0530

    EXLM-1376 Page meta-tags for coveo index

--- coveo/generate-coveo-index.js
+++ coveo/generate-coveo-index.js
@@ -7,72 +7,65 @@ const args = process.argv.slice(2);
 const languageIndex = args.indexOf('--language');
 const language = languageIndex !== -1 ? args[languageIndex + 1] : 'en';
 
-// Fetch articles
 async function fetchDataFromURL(url) {
-    return new Promise((resolve, reject) => {
-        https.get(url, (response) => {
-            let data = '';
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (response) => {
+        let data = '';
 
-            // A chunk of data has been received.
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            // The whole response has been received.
-            response.on('end', () => {
-                try {
-                    const jsonData = JSON.parse(data);
-                    resolve(jsonData);
-                } catch (error) {
-                    reject(error);
-                }
-            });
-        }).on('error', (error) => {
-            reject(error);
+        // A chunk of data has been received.
+        response.on('data', (chunk) => {
+          data += chunk;
         });
-    });
+
+        // The whole response has been received.
+        response.on('end', () => {
+          try {
+            const jsonData = JSON.parse(data);
+            resolve(jsonData);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
 }
 
-// Example usage:
 const url = `https://main--franklin-exlm--ruhisingh1.hlx.page/${language}/article-index.json`;
-fetchDataFromURL(url)
-    .then(data => {
-        console.log('Data:', data);
-    })
-    .catch(err => {
-        console.error('Error:', err);
-    });
 
 // Function to decode base64 strings
 function decodeBase64(encodedString) {
-    return Buffer.from(encodedString, 'base64').toString('utf-8');
+  return Buffer.from(encodedString, 'base64').toString('utf-8');
 }
 
 // Process articles and generate XML content
 async function generateXmlContent() {
-    try {
-        // Call fetchDataFromURL instead of fetchArticles
-        const articles = await fetchDataFromURL(url);
-        console.log(articles);
-        const xmlData = [];
+  try {
+    // Call fetchDataFromURL instead of fetchArticles
+    const articles = await fetchDataFromURL(url);
+    const xmlData = [];
 
-        articles.data.forEach((article) => { // Access 'data' property of articles
-            xmlData.push('<url>');
-            xmlData.push(`  <loc>${article.path}</loc>`);
-            xmlData.push(`  <lastmod>${article.lastModified}</lastmod>`); // Fix property name
-            xmlData.push('  <changefreq>daily</changefreq>');
-            xmlData.push('  <coveo:metadata>');
-            xmlData.push(`    <coveo-content-type>${article.coveoContentType}</coveo-content-type>`);
-            xmlData.push(`    <coveo-solution>${decodeBase64(article.coveoSolution)}</coveo-solution>`);
-            xmlData.push(`    <role>${decodeBase64(article.coveoRole)}</role>`); // Fix property name
-            xmlData.push(`    <level>${decodeBase64(article.coveoLevel)}</level>`); // Fix property name
-            xmlData.push(`    <author-type>${article.authorType}</author-type>`); // Make sure article object has 'authorType' property
-            xmlData.push(`    <author-name>${article.authorName}</author-name>`); // Make sure article object has 'authorName' property
-            xmlData.push('  </coveo:metadata>');
-            xmlData.push('</url>');
-        });
+    articles.data.forEach((article) => {
+      // Access 'data' property of articles
+      xmlData.push('<url>');
+      xmlData.push(`  <loc>${article.path}</loc>`);
+      xmlData.push(`  <lastmod>${article.lastModified}</lastmod>`);
+      xmlData.push('  <changefreq>daily</changefreq>');
+      xmlData.push('  <coveo:metadata>');
+      xmlData.push(`    <coveo-content-type>${article.coveoContentType}</coveo-content-type>`);
+      xmlData.push(`    <coveo-solution>${decodeBase64(article.coveoSolution)}</coveo-solution>`);
+      xmlData.push(`    <role>${decodeBase64(article.coveoRole)}</role>`);
+      xmlData.push(`    <level>${decodeBase64(article.coveoLevel)}</level>`);
+      xmlData.push(`    <author-type>${article.authorType}</author-type>`);
+      xmlData.push(`    <author-name>${article.authorName}</author-name>`);
+      xmlData.push('  </coveo:metadata>');
+      xmlData.push('</url>');
+    });
 
-        return `
+    return `
             <urlset xmlns="http://www.google.com/schemas/sitemap/0.84"
                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
                     xmlns:coveo="http://www.coveo.com/schemas/metadata"
@@ -80,34 +73,34 @@ async function generateXmlContent() {
                 ${xmlData.join('\n')}
             </urlset>
         `;
-    } catch (error) {
-        console.error('Error generating XML content:', error);
-        throw error;
-    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error generating XML content:', error);
+    throw error;
+  }
 }
 
 // Write Coveo XML file
 async function writeCoveoXML() {
-    try {
-        const xmlContent = await generateXmlContent(); // Access 'url' from the outer scope
-        const fileName = `coveo_${language}.xml`;
-        console.log('Writing to file:', fileName);
-        fs.writeFileSync(fileName, xmlContent);
-        console.log(`Coveo XML file '${fileName}' created successfully.`);
-    } catch (error) {
-        console.error('Error writing Coveo XML file:', error);
-        throw error;
-    }
+  try {
+    const xmlContent = await generateXmlContent();
+    const fileName = `coveo_${language}.xml`;
+    fs.writeFileSync(fileName, xmlContent);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error writing Coveo XML file:', error);
+    throw error;
+  }
 }
 
 // Main function
 async function main() {
-    try {
-        await writeCoveoXML();
-    } catch (error) {
-        console.error('Error:', error);
-    }
+  try {
+    await writeCoveoXML();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error:', error);
+  }
 }
 
-// Execute main function
 main();
