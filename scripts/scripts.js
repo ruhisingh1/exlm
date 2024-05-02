24b92c8293508323cdfcf1ee5f92e79c9ac807bc
ruhisingh1 <145125444+ruhisingh1@users.noreply.github.com>
2024-05-02 17:47:00 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -240,7 +240,7 @@ function buildAutoBlocks(main) {
       addArticleLandingRail(main);
     }
     // eslint-disable-next-line no-use-before-define
-    addMiniTocForArticlesPage(main);
+    // addMiniTocForArticlesPage(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
