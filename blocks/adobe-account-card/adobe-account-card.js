245506706af6eb371d6a2aac8f70eb2fc7c45d48
Ruhi Singh <ruhis@adobe.com>
2024-05-21 22:42:17 +0530

    account card

--- blocks/adobe-account-card/adobe-account-card.js
+++ blocks/adobe-account-card/adobe-account-card.js
@@ -12,41 +12,40 @@ const adobeAccountLink = 'https://account.adobe.com/';
 const ADOBE_ACCOUNT = placeholders?.myAdobeAccount || 'My Adobe Account';
 const MAKE_CHANGES_TEXT = placeholders?.makeChanges || 'Make changes';
 
-let displayName = '';
-let email = '';
-let company = '';
-let profileImg = ''; // default initials or placeholder
-
-const isSignedIn = await isSignedInUser();
-if (isSignedIn) {
-  const profileData = JSON.parse(sessionStorage.getItem('profile')) || {};
-  displayName = profileData.displayName || '';
-  email = profileData.email || '';
-  company = profileData.company || '';
-  profileImg = await defaultProfileClient.getPPSProfile().then((ppsProfile) => {
-    const profilePicture = ppsProfile?.images['50'];
-    if (profilePicture) {
-      return `<img class="profile-picture" src="${profilePicture}" alt="profile picture" />`;
-    }
-  });
-}
-
 export default async function decorate(block) {
   const accountCardDOM = document.createRange().createContextualFragment(`
     <div class="card-header">
         <div class="adobe-account">${ADOBE_ACCOUNT}</div>
-        <div class="make-changes"><span class="icon icon-link-out"></span><a href="${adobeAccountLink}" target="_blank">${MAKE_CHANGES_TEXT}</a></div>
+        <div class="make-changes"><span class="icon icon-new-tab"></span><a href="${adobeAccountLink}" target="_blank">${MAKE_CHANGES_TEXT}</a></div>
       </div>
       <div class="card-body">
-        <div class="avatar">${profileImg}</div>
+        <div class="avatar"><div class="profile"></div></div>
         <div class="user-info">
-          <div class="display-name">${displayName}</div>
-          <div class="company">${company}</div>
-          <div class="email">${email}</div>
+          <div class="display-name">Username</div>
+          <div class="company">Company</div>
+          <div class="email">Email</div>
         </div>
       </div>
   `);
 
   block.textContent = '';
   block.append(accountCardDOM);
+
+  const isSignedIn = await isSignedInUser();
+  if (isSignedIn) {
+    const profileData = JSON.parse(sessionStorage.getItem('profile')) || {};
+    const displayName = profileData.displayName || '';
+    const email = profileData.email || '';
+    const company = profileData.company || '';
+    const profileImg = await defaultProfileClient.getPPSProfile().then((ppsProfile) => {
+      const profilePicture = ppsProfile?.images['50'];
+      if (profilePicture) {
+        return `<img class="profile-picture" src="${profilePicture}" alt="profile picture" />`;
+      }
+    });
+    block.querySelector('.display-name').innerHTML = displayName;
+    block.querySelector('.company').innerHTML = company;
+    block.querySelector('.email').innerHTML = email;
+    block.querySelector('.profile').innerHTML = profileImg;
+  }
 }
