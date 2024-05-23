474a435b1ca4f96c7d42a80a92f4a9a65a6c2843
Ruhi Singh <ruhis@adobe.com>
2024-05-23 21:27:19 +0530

    user-profile-card

--- blocks/user-profile-card/user-profile-card.js
+++ blocks/user-profile-card/user-profile-card.js
@@ -31,11 +31,11 @@ export default async function decorate(block) {
     const profileInterests = profileData?.interests || '';
 
     if (profileRoles.length > 0) {
-      roles = profileRoles.join(' ');
+      roles = profileRoles.join('  ');
     }
     
     if (profileInterests.length > 0) {
-      interests = profileInterests.join(' ');
+      interests = profileInterests.join('  ');
     }
 
     const ppsProfileData = await defaultProfileClient.getPPSProfile();
