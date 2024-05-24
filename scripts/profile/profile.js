8645ff922bb07d10a30209584faf18ad752a4ec9
Ruhi Singh <ruhis@adobe.com>
2024-05-24 14:42:42 +0530

    user-profile-card

--- scripts/profile/profile.js
+++ scripts/profile/profile.js
@@ -8,6 +8,7 @@ try {
   /* eslint-disable-next-line no-console */
   console.error('Error fetching placeholders:', err);
 }
+
 const adobeAccountLink = 'https://account.adobe.com/';
 const communityAccountLink = '';
 
@@ -42,62 +43,48 @@ if (isSignedIn) {
 }
 
 export const adobeAccountDOM = `<div class="row adobe-account">
-        <div class="card-header">
-          <div class="my-adobe-account">${placeholders?.myAdobeAccount || 'My Adobe Account'}</div>
-          <div class="manage-adobe-account">
-            <span class="icon icon-new-tab"></span>
-            <a href="${adobeAccountLink}" target="_blank">${
-              placeholders?.manageAdobeAccount || 'Manage Adobe account'
-            }</a>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="avatar">
-            ${
-              profilePicture
-                ? `<img class="profile-picture" src="${profilePicture}" alt="profile picture" />`
-                : '<span class="icon icon-profile"></span>'
-            }
-          </div>
-          <div class="user-info">
-            <div class="display-name">${displayName}</div>
-            <div>${company}</div>
-            <div>${email}</div>
-          </div>
-        </div>
-      </div>`;
+  <div class="card-header adobe-account-header">
+    <div class="my-adobe-account">${placeholders?.myAdobeAccount || 'My Adobe Account'}</div>
+    <div class="manage-adobe-account">
+      <span class="icon icon-new-tab"></span>
+      <a href="${adobeAccountLink}" target="_blank">${placeholders?.manageAdobeAccount || 'Manage Adobe account'}</a>
+    </div>
+  </div>
+  <div class="card-body adobe-account-body">
+    <div class="avatar">
+      ${profilePicture ? `<img class="profile-picture" src="${profilePicture}" alt="Profile Picture" />` : '<span class="icon icon-profile"></span>'}
+    </div>
+    <div class="user-info">
+      <div class="display-name adobe-display-name">${displayName}</div>
+      <div class="user-company">${company}</div>
+      <div class="user-email">${email}</div>
+    </div>
+  </div>
+</div>`;
 
 export const communityAccountDOM = `<div class="row community-account">
-      <div class="card-header">
-        <div class="my-community-account">${placeholders?.myCommunityAccount || 'My Community Profile'}</div>
-        <div class="manage-community-account">
-          <span class="icon icon-new-tab"></span>
-          <a href="${communityAccountLink}" target="_blank">${
-            placeholders?.updateCommunityProfile || 'Update profile'
-          }</a>
-        </div>
-      </div>
-      <div class="card-body">
-        <div class="user-info">
-          <div class="community-display-name">@RyPot478</div>
-          <div><span class="heading">${
-            placeholders?.title || 'Title'
-          }: </span><span>Head of Experience Design</span></div>
-          <div><span class="heading">${
-            placeholders?.location || 'Location'
-          }: </span><span>Salt Lake City, UT</span></div>
-        </div>
-      </div>
-    </div>`;
+  <div class="card-header community-account-header">
+    <div class="my-community-account">${placeholders?.myCommunityAccount || 'My Community Profile'}</div>
+    <div class="manage-community-account">
+      <span class="icon icon-new-tab"></span>
+      <a href="${communityAccountLink}" target="_blank">${placeholders?.updateCommunityProfile || 'Update profile'}</a>
+    </div>
+  </div>
+  <div class="card-body community-account-body">
+    <div class="user-info">
+      <div class="display-name community-display-name">@RyPot478</div>
+      <div class="community-title"><span class="heading">${placeholders?.title || 'Title'}: </span><span>Head of Experience Design</span></div>
+      <div class="community-location"><span class="heading">${placeholders?.location || 'Location'}: </span><span>Salt Lake City, UT</span></div>
+    </div>
+  </div>
+</div>`;
 
 export const additionalProfileInfoDOM = `<div class="row additional-data">
-    <div class="card-body">
-      <div class="user-info">
-        <div><span class="heading">${placeholders?.myRole || 'My Role'}: </span><span>${roles}</span></div>
-        <div><span class="heading">${placeholders?.myIndustry || 'My Industry'}: </span><span>${industry}</span></div>
-        <div><span class="heading">${
-          placeholders?.myInterests || 'My interests'
-        }: </span><span>${interests}</span></div>
-      </div>
+  <div class="card-body additional-data-body">
+    <div class="user-info">
+      <div class="user-role"><span class="heading">${placeholders?.myRole || 'My Role'}: </span><span>${roles}</span></div>
+      <div class="user-industry"><span class="heading">${placeholders?.myIndustry || 'My Industry'}: </span><span>${industry}</span></div>
+      <div class="user-interests"><span class="heading">${placeholders?.myInterests || 'My interests'}: </span><span>${interests}</span></div>
     </div>
-  </div>`;
+  </div>
+</div>`;
