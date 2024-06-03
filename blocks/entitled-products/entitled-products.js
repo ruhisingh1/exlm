a8a51bd0ebe45ccd49a1b1bcec5551b257daa1f9
Ruhi Singh <ruhis@adobe.com>
2024-06-03 21:59:08 +0530

    EXLM-1575 Product Interests blocks

--- blocks/entitled-products/entitled-products.js
+++ blocks/entitled-products/entitled-products.js
@@ -2,44 +2,38 @@ import { decorateIcons, loadCSS } from '../../scripts/lib-franklin.js';
 import { sendNotice } from '../../scripts/toast/toast.js';
 import { getConfig } from '../../scripts/scripts.js';
 import { defaultProfileClient, isSignedInUser } from '../../scripts/auth/profile.js';
+import buildProductCard from '../../scripts/profile/profile-interests.js';
 import eventHandler from '../../scripts/profile/profile-interests-event.js';
 
 loadCSS(`${window.hlx.codeBasePath}/scripts/toast/toast.css`);
 
-const { jilAPi, ims } = getConfig();
-
-export default async function decorate(block) {
-  const [...configs] = [...block.children].map((row) => row.firstElementChild);
-
-  const [headingElement, descElement] = configs.map((cell) => cell);
-  const [headingContent, descContent] = configs.map((cell) => cell?.textContent?.trim() ?? '');
-  const styledHeader =
-    headingElement.firstElementChild === null ? `<h3>${headingContent}</h3>` : headingElement.innerHTML;
-
+export default async function decorate(block, model) {
+  const [headingElement, descElement] = [...block.children].map((row) => row.firstElementChild);
+  const headingContent = headingElement?.textContent?.trim() ?? '';
+  const descContent = descElement?.textContent?.trim() ?? '';
+  
+  const styledHeader = headingElement.firstElementChild === null 
+    ? `<h3>${headingContent}</h3>` 
+    : headingElement.innerHTML;
+  
   const entitlementsDOM = document.createRange().createContextualFragment(`
-  ${styledHeader !== '' ? `<div class="heading">${styledHeader}</div>` : ``}
-    ${descElement !== '' ? `<div class="description">${descContent}</div>` : ``}
-      <div class="product-card">
-      <span class="checkbox">
-      <input data-name="Analytics" type="checkbox">
-      <label for="Analytics" class="subtext">Analytics</label>
-    </span><span class="checkbox">
-    <input data-name="Target" type="checkbox">
-    <label for="Target" class="subtext">Target</label>
-  </span>
-      </div>
+    ${styledHeader ? `<div class="heading">${styledHeader}</div>` : ''}
+    ${descContent ? `<div class="description">${descContent}</div>` : ''}
   `);
-
+  
+  entitlementsDOM.appendChild(buildProductCard(block, model));
+  
   block.innerHTML = '';
   block.append(entitlementsDOM);
+  
   eventHandler();
   // await decorateIcons(block);
 
   // const isSignedIn = await isSignedInUser();
   // if (isSignedIn) {
-  //     const profileData = await defaultProfileClient.getMergedProfile();
-  //     const solutionLevels = profileData?.solutionLevels || [];
-  //     defaultProfileClient
+  //   const profileData = await defaultProfileClient.getMergedProfile();
+  //   const solutionLevels = profileData?.solutionLevels || [];
+  //   defaultProfileClient
   //     .updateProfile("solutionLevels", isChecked)
   //     .then(() => sendNotice(PROFILE_UPDATED))
   //     .catch(() => sendNotice(PROFILE_NOT_UPDATED));
