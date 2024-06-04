38c58aa5c2b4ee9098926fe71ea80260e8ac83a9
Ruhi Singh <ruhis@adobe.com>
2024-06-04 19:06:11 +0530

    EXLM-1575 Product Interests blocks

--- blocks/entitled-products/entitled-products.js
+++ blocks/entitled-products/entitled-products.js
@@ -1,53 +1,100 @@
-import { decorateIcons, loadCSS } from '../../scripts/lib-franklin.js';
+/* eslint-disable no-lonely-if */
+import { loadCSS } from '../../scripts/lib-franklin.js';
 import { sendNotice } from '../../scripts/toast/toast.js';
-import { getConfig } from '../../scripts/scripts.js';
 import { defaultProfileClient, isSignedInUser } from '../../scripts/auth/profile.js';
 import buildProductCard from '../../scripts/profile/profile-interests.js';
 import eventHandler from '../../scripts/profile/profile-interests-event.js';
+import { htmlToElement, fetchLanguagePlaceholders } from '../../scripts/scripts.js';
 
 loadCSS(`${window.hlx.codeBasePath}/scripts/toast/toast.css`);
 
-export default async function decorate(block, model) {
+let placeholders = {};
+try {
+  placeholders = await fetchLanguagePlaceholders();
+} catch (err) {
+   // eslint-disable-next-line no-console
+  console.error('Error fetching placeholders:', err);
+}
+
+export default async function decorate(block) {
+
   const [headingElement, descElement] = [...block.children].map((row) => row.firstElementChild);
-  const headingContent = headingElement?.textContent?.trim() ?? '';
-  const descContent = descElement?.textContent?.trim() ?? '';
-  
-  const styledHeader = headingElement.firstElementChild === null 
-    ? `<h3>${headingContent}</h3>` 
-    : headingElement.innerHTML;
-  
-  const entitlementsDOM = document.createRange().createContextualFragment(`
-    ${styledHeader ? `<div class="heading">${styledHeader}</div>` : ''}
-    ${descContent ? `<div class="description">${descContent}</div>` : ''}
-  `);
-  
-  const { product = 'Target', isSelected = false } = model;
-  console.log(`Product: ${product}, Is Selected: ${isSelected}`);
-  
-  const productCard = await buildProductCard(block, model);
-  entitlementsDOM.appendChild(productCard);
-  
+  const headingContent = headingElement?.textContent?.trim() || '';
+  const descContent = descElement?.textContent?.trim() || '';
+
+  const styledHeader = headingElement?.firstElementChild === null ? `<h3>${headingContent}</h3>` : headingElement.innerHTML;
+
   block.innerHTML = '';
-  block.append(entitlementsDOM);
+
+  const headerDiv = htmlToElement(`
+    <div class="block-header">
+      <div class="block-title">${styledHeader}</div>
+      ${descContent !== '' ? `<div class="block-description">${descElement.innerHTML}</div>` : ''}
+    </div>
+  `);
+  block.appendChild(headerDiv);
+ 
+  const contentDiv = document.createElement('div');
+  contentDiv.classList.add('block-cards-content');
+
+  // Array of products with selection status
+  const products = [
+    { product: 'Analytics', isSelected: true },
+    { product: 'Workfront', isSelected: true },
+    { product: 'Target', isSelected: false },
+  ];
   
-  eventHandler();
-  // await decorateIcons(block);
+  products.forEach((product) => {
+    buildProductCard(block, contentDiv, product);
+  });
 
-  // const isSignedIn = await isSignedInUser();
-  // if (isSignedIn) {
-  //   const profileData = await defaultProfileClient.getMergedProfile();
-  //   const solutionLevels = profileData?.solutionLevels || [];
-  //   defaultProfileClient
-  //     .updateProfile("solutionLevels", isChecked)
-  //     .then(() => sendNotice(PROFILE_UPDATED))
-  //     .catch(() => sendNotice(PROFILE_NOT_UPDATED));
-  // }
+  block.appendChild(contentDiv);
 
-  // if (isSignedIn) {
-  //   const preferenceName = checkbox.getAttribute('data-name');
-  //   defaultProfileClient
-  //     .updateProfile(preferenceName, isChecked)
-  //     .then(() => sendNotice(PROFILE_UPDATED))
-  //     .catch(() => sendNotice(PROFILE_NOT_UPDATED));
-  // }
+  // eventHandler();
+
+  const isSignedIn = await isSignedInUser();
+  if (isSignedIn) {
+    const profileData = await defaultProfileClient.getMergedProfile();
+    const solutionLevels = profileData?.solutionLevels || [];
+
+    solutionLevels.forEach((el) => {
+        const solutionName = el.split(',')[0];
+        const checkBox = block.querySelector(`input[data-name="${solutionName}"]`);
+        if (checkBox) {
+            checkBox.checked = true;
+            checkBox.closest('.profile-interest-card').classList.add('profile-interest-card--selected');
+        }
+    });
+}
+
+  const updatedSolutionLevels = [];
+  block.querySelectorAll('.profile-interest-card').forEach((card) => {
+    const checkbox = card.querySelector('input[type="checkbox"]');
+    const dropdown = card.querySelector('.custom-filter-dropdown');
+
+    checkbox.addEventListener('change', () => {
+      if (isSignedIn) {
+        const solution = checkbox.getAttribute('data-name');
+        const level = dropdown.getAttribute('data-selected');
+        const index = updatedSolutionLevels.findIndex((item) => item.solution === solution);
+        if (checkbox.checked) {
+          // Add or update the solution level
+          if (index !== -1) {
+            updatedSolutionLevels[index].level = level;
+          } else {
+            updatedSolutionLevels.push({ solution, level });
+          }
+        } else {
+          // Remove the solution level if unchecked
+          if (index !== -1) {
+            updatedSolutionLevels.splice(index, 1);
+          }
+        }
+        defaultProfileClient
+          .updateProfile("solutionLevels", updatedSolutionLevels)
+          .then(() => sendNotice(placeholders?.profileUpdated || 'Your profile changes have been saved!'))
+          .catch(() => sendNotice(placeholders?.profileNotUpdated || 'Your profile changes have not been saved!'));
+      }
+    });
+  });
 }
--- scripts/profile/profile-interests.js
+++ scripts/profile/profile-interests.js
@@ -1,28 +1,37 @@
 import { decorateIcons, loadCSS } from '../lib-franklin.js';
 import Dropdown from '../dropdown/dropdown.js';
-import { htmlToElement } from '../scripts.js';
+import { htmlToElement, fetchLanguagePlaceholders } from '../scripts.js';
 import getSolutionByName from '../../blocks/toc/toc-solutions.js';
 
-loadCSS(`${window.hlx.codeBasePath}/scripts/profile-interests/profile-interests.css`);
+loadCSS(`${window.hlx.codeBasePath}/scripts/profile/profile-interests.css`);
+loadCSS(`${window.hlx.codeBasePath}/scripts/toast/toast.css`);
+
+let placeholders = {};
+try {
+  placeholders = await fetchLanguagePlaceholders();
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.error('Error fetching placeholders:', err);
+}
 
 const options = [
   {
     value: 'Beginner',
-    title: 'Beginner',
+    title: placeholders.profileExpLevelBeginner || 'Beginner',
   },
   {
     value: 'Intermediate',
-    title: 'Intermediate',
+    title: placeholders.profileExpLevelIntermediate || 'Intermediate',
   },
   {
     value: 'Advanced',
-    title: 'Advanced',
+    title: placeholders.profileExpLevelExperienced || 'Experienced',
   },
 ];
 
 // eslint-disable-next-line import/prefer-default-export
-export default async function buildProductCard(element, model) {
-  const { product = 'Target', isSelected = false } = model;
+export default async function buildProductCard(container, element, model) {
+  const { product = 'Acrobat', isSelected = false } = model;
 
   // Create card container
   const card = document.createElement('div');
@@ -33,20 +42,22 @@ export default async function buildProductCard(element, model) {
   const header = htmlToElement(`
         <div class="profile-interest-header">
             <div class="profile-interest-logo-wrapper">
-                <img class="profile-interest-logo">
-                <span class="profile-interest-logo-text">My Adobe Product</span>
+                <span class="icon profile-interest-logo"></span>
+                <span class="profile-interest-logo-text">${placeholders.myAdobeproduct || 'My Adobe product'}</span>
             </div>
             <h3>${product}</h3>
         </div>
     `);
 
-  const img = header.querySelector('img');
-  img.src = `/icons/solutions/${solutionInfoClassName}.svg`;
+  const iconEl = header.querySelector('span');
+  iconEl.classList.add(`icon-${solutionInfoClassName}`);
 
   // Content
   const content = htmlToElement(`
         <div class="profile-interest-content">
-            <label for="experience-level">Select your experience level</label>
+            <label for="experience-level">${
+              placeholders.selectYourExperienceLevel || 'Select your experience level'
+            }</label>
         </div>
     `);
 
@@ -54,31 +65,44 @@ export default async function buildProductCard(element, model) {
   new Dropdown(content, 'Beginner', options);
 
   // Checkbox
-  const checboxCta = 'Select this product';
-  const changeHandler = (e) => {
-    const { checked } = e.target;
-    if (checked) {
-      card.classList.add('profile-interest-card--selected');
-    } else {
-      card.classList.remove('profile-interest-card--selected');
-    }
-  };
   const checkboxContainer = htmlToElement(`
         <div class="profile-interest-checkbox">
-            <input type="checkbox" id="select-product">
-            <label for="select-product">${checboxCta}</label>
+            <input type="checkbox" data-name="${product}">
+            <label for="${product}" class="subtext">${placeholders.selectThisProduct || 'Select this product'}</label>
         </div>`);
+
   const checkbox = checkboxContainer.querySelector('input');
   checkbox.checked = isSelected;
-  checkbox.onchange = changeHandler;
+
+  // Change handler function
+  const changeHandler = () => {
+    const { checked } = checkbox;
+    card.classList.toggle('profile-interest-card--selected', checked);
+  };
+
+  // Click event listener for the entire card
+  card.addEventListener('click', () => {
+    checkbox.checked = !checkbox.checked;
+    changeHandler();
+  });
+
+  // Click event listener for the checkbox
+  checkbox.addEventListener('change', () => {
+    changeHandler();
+  });
+
+  // Prevent checkbox click from propagating to card
+  checkbox.addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
 
   // Assemble card
   card.appendChild(header);
+  decorateIcons(header, 'solutions/');
   card.appendChild(content);
+  decorateIcons(content);
   card.appendChild(checkboxContainer);
 
   // Add to DOM
   element.appendChild(card);
-  decorateIcons(element);
-  return card;
 }
