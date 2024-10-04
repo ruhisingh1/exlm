bc9f88567eb4ec7c5c05d17f6e1aa11f37d61a38
Ruhi Singh <ruhis@adobe.com>
2024-10-04 18:19:52 +0530

    incomplete modal

--- scripts/signup-flow/signup-flow-handler.js
+++ scripts/signup-flow/signup-flow-handler.js
@@ -21,9 +21,7 @@ export default async function showSignupDialog() {
   const configDate = new Date(signUpFlowConfigDate);
   const profileData = await defaultProfileClient.getMergedProfile();
   const interests = profileData.interests ?? [];
-  const interactions = profileData.interactions ?? [];
   const profileTimeStamp = new Date(profileData.timestamp);
-  const modalSeenEventIndex = interactions.findIndex((interaction) => interaction.event === 'modalSeen');
   const modalSeenInteraction = await defaultProfileClient.getLatestInteraction('modalSeen');
 
   const todayStartTimeStamp = new Date();
@@ -33,19 +31,30 @@ export default async function showSignupDialog() {
     const modalSeenTimeStamp = new Date(modalSeenInteraction.timestamp);
     const pastDate = new Date(todayStartTimeStamp);
     pastDate.setDate(todayStartTimeStamp.getDate() - modalReDisplayDuration);
-
-    // Display modal again if no interests and time since last modal shown has passed the duration
-    // if (interests.length === 0 && modalSeenTimeStamp < pastDate) {
-      if (interests.length > 0) {
+  
+    // Display modal again if no interests and modal timestamp is older than the past date
+    if (interests.length === 0 && modalSeenTimeStamp < pastDate) {
       SignupFlowDialog.init(SIGNUP_DIALOG_TYPE.INCOMPLETE_PROFILE);
-
-      // Update modalSeen timestamp
+  
+      // Update modalSeen timestamp only
       const currentTimestamp = new Date().toISOString();
-      interactions[modalSeenEventIndex].timestamp = currentTimestamp;
-      // Save the updated interaction back to the profile
-      await defaultProfileClient.updateProfile('interactions', interactions);
+  
+      // Fetch all interactions from the profile
+      const interactions = profileData.interactions ?? [];
+  
+      // Find the existing modalSeen event in the interactions array
+      const modalSeenEvent = interactions.find((interaction) => interaction.event === 'modalSeen');
+  
+      if (modalSeenEvent) {
+        // Update the timestamp of the found modalSeen event
+        modalSeenEvent.timestamp = currentTimestamp;
+  
+        // Save the updated interactions array back to the profile
+        await defaultProfileClient.updateProfile('interactions', interactions);
+      }
     }
-  } else {
+  }
+   else {
     // eslint-disable-next-line no-lonely-if
     if (profileTimeStamp < todayStartTimeStamp) {
       SignupFlowDialog.init(SIGNUP_DIALOG_TYPE.INCOMPLETE_PROFILE);
