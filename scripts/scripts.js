73f0cd61d2916b19dd136888b4a3c4a05f22bc5d
Ruhi Singh <ruhis@adobe.com>
2025-01-29 13:33:53 +0530

    conflicts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -522,11 +522,11 @@ export function decorateMain(main) {
   decorateSections(main);
   decorateBlocks(main);
   buildSectionBasedAutoBlocks(main);
-  const lastSection = main.querySelector(".section[last-section='true']");
+  const lastSection = main.querySelector(".section[data-last-section='true']");
   const profileRailSection = main.querySelector('.profile-rail-section');
-  if (profileRailSection) {
+  if (profileRailSection && lastSection) {
     main.insertBefore(lastSection, profileRailSection);
-  } else {
+  } else if (lastSection) {
     main.appendChild(lastSection);
   }
 }
