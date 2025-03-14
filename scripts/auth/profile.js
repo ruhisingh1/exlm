964fd73d3cb20f1aa2e50823d6dc92325f9e7094
Ruhi Singh <ruhis@adobe.com>
2025-03-14 19:39:04 +0530

    EXLM-3090 Sign out redirect URL

--- scripts/auth/profile.js
+++ scripts/auth/profile.js
@@ -26,18 +26,15 @@ export async function isSignedInUser() {
 /**
  * @see: https://git.corp.adobe.com/IMS/imslib2.js#documentation
  * @see: https://wiki.corp.adobe.com/display/ims/IMS+API+-+logout#IMSApi-logout-signout_options
- * @param {Object} [signoutOptions]
- * @param {string} [signoutOptions.redirect_uri] - A URL to which the user agent is redirected on successful logout.
  */
-export async function signOut(signoutOptions) {
+export async function signOut() {
   ['JWT', 'coveoToken', 'attributes', 'exl-profile', 'profile', 'pps-profile'].forEach((key) =>
     sessionStorage.removeItem(key),
   );
   const signOutRedirectUrl = getMetadata('signout-redirect-url');
-  if(signOutRedirectUrl){
-    signoutOptions.redirect_uri = signOutRedirectUrl;
-  }
-  if (signoutOptions) {
+
+  if (signOutRedirectUrl) {
+    const signoutOptions = { redirect_uri: signOutRedirectUrl };
     window.adobeIMS?.signOut(signoutOptions);
   } else {
     window.adobeIMS?.signOut();
